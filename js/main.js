1886d6092d11b051aacb9da9d9588ed22f900093
Henrique Beckmann <henriquebeckmann2@gmail.com>
2023-03-08 00:31:43 -0300

    Patricia linktree project initialized

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.whatsapp').addEventListener('click', () => {
+  window.open('https://wa.me/5555997324811');
+})
+
+document.querySelector('.location').addEventListener('click', () => {
+  window.open('https://maps.app.goo.gl/nH8bAgSMBjKqD2B37?g_st=iw');
+})
+
+document.querySelector('.evaluate').addEventListener('click', () => {
+  window.open('https://maps.app.goo.gl/nH8bAgSMBjKqD2B37?g_st=iw');
+})
